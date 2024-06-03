232e9e1df72f6df432a6e59cd4629032c8c8ea59
MonDust <82498819+MonDust@users.noreply.github.com>
2024-06-03 22:41:11 +0200

    Update model.py

--- model.py
+++ model.py
@@ -42,7 +42,7 @@ def load_dataset(words_dir, words, output_sequence_length=16000):
 
     return file_paths, labels
 
-# Function to preprocess dataset using tf.data
+# Function to preprocess dataset
 def preprocess_dataset(file_paths, labels, batch_size=64):
     def load_and_preprocess(file_path, label):
         spectrogram = load_and_preprocess_audio(file_path)
@@ -57,11 +57,8 @@ def preprocess_dataset(file_paths, labels, batch_size=64):
 
     return dataset
 
-# Load words
 with open(r'C:\Users\maria\Desktop\PG\SEM_IV\Artificial_Intelligence\Project\PSD\words_useful2.txt', 'r', encoding='utf-8') as file:
     words = file.read().splitlines()
-
-# Directory containing words
 words_dir = r'C:\Users\maria\Desktop\PG\SEM_IV\Artificial_Intelligence\Project\PSD\words'
 maximum_number_of_words = len(words)
 number_of_words = 50
@@ -69,10 +66,8 @@ if maximum_number_of_words < number_of_words:
     number_of_words = maximum_number_of_words
 words = words[:number_of_words]
 
-# Load data
 file_paths, labels = load_dataset(words_dir, words)
 
-# One-hot encode labels
 label_encoder = LabelEncoder()
 labels_encoded = label_encoder.fit_transform(labels)
 labels_one_hot = to_categorical(labels_encoded)
@@ -81,18 +76,16 @@ labels_one_hot = to_categorical(labels_encoded)
 X_train, X_temp, y_train, y_temp = train_test_split(file_paths, labels_one_hot, test_size=0.3, random_state=42)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
-# Normalize the data
 norm_layer = Normalization()
 dummy_spectrogram = load_and_preprocess_audio(file_paths[0])
 dummy_spectrogram = tf.expand_dims(dummy_spectrogram, axis=0)
 norm_layer.adapt(dummy_spectrogram)
 
-# Prepare datasets
 train_dataset = preprocess_dataset(X_train, y_train)
 val_dataset = preprocess_dataset(X_val, y_val)
 test_dataset = preprocess_dataset(X_test, y_test)
 
-# Define the model architecture
+# Model
 model = Sequential()
 model.add(Input(shape=(dummy_spectrogram.shape[1], dummy_spectrogram.shape[2], 1)))
 model.add(norm_layer)
@@ -109,13 +102,10 @@ model.add(Dense(len(words), activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Define callbacks
 early_stopping = EarlyStopping(monitor='val_loss', patience=4, restore_best_weights=True)
 model_checkpoint = ModelCheckpoint('best_model.keras', monitor='val_accuracy', save_best_only=True)
 
-# Train the model
 history = model.fit(train_dataset, epochs=40, validation_data=val_dataset, callbacks=[early_stopping, model_checkpoint])
 
-# Evaluate the model
 test_loss, test_acc = model.evaluate(test_dataset)
 print('Test accuracy:', test_acc)
